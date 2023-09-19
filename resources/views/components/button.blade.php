b3aa2c6da473bbdd8919ede91f78c7a8618f9c36
Andri Sunardi <info@andrisunardi.com>
2023-09-19 12:50:52 +0700

    Move File to Public From V1

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -14,17 +14,20 @@
     @if ($type != 'submit') wire:click="{{ $key }}" @endif wire:loading.attr="disabled"
     @if ($confirm) onclick="return confirm('{{ $confirm }} ?') || event.stopImmediatePropagation()" @endif>
 
-    @if ($icon && $position == 'left')
-        <span class="{{ $icon }} fa-fw"></span>
-    @endif
+    <div wire:loading.remove wire:target="{{ $key }}">
+        @if ($icon && $position == 'left')
+            <span class="{{ $icon }} fa-fw"></span>
+        @endif
 
-    {{ $text }}
+        {{ $text }}
 
-    @if ($icon && $position == 'right')
-        <span class="{{ $icon }} fa-fw"></span>
-    @endif
+        @if ($icon && $position == 'right')
+            <span class="{{ $icon }} fa-fw"></span>
+        @endif
+    </div>
 
     <div wire:loading wire:target="{{ $key }}">
-        <span class="fas fa-spinner fa-spin"></span>
+        <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+        <span role="status">{{ trans('index.loading') }}...</span>
     </div>
 </button>
