af40b5f0547029b82a82ecbd819fa675718fdc60
Andri Sunardi <info@andrisunardi.com>
2023-09-21 23:09:43 +0700

    Add Components Create Store Update

---
+++ database/migrations/2022_01_01_000000_create_banners_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pet_id')->nullable()->unsigned();
+            $table->string('image', 130)->nullable();
+            $table->boolean('is_active')->nullable()->unsigned()->default(true);
+            $table->foreignId('created_by_id')->nullable()->unsigned();
+            $table->foreignId('updated_by_id')->nullable()->unsigned();
+            $table->foreignId('deleted_by_id')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
---
+++ database/seeders/BannerSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Banner;
+use Illuminate\Database\Seeder;
+
+class BannerSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $data = new Banner();
+        $data->pet_id = 1;
+        $data->image = "dog-1.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 1;
+        $data->image = "dog-2.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 1;
+        $data->image = "dog-3.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 1;
+        $data->image = "dog-4.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 2;
+        $data->image = "cat-1.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 2;
+        $data->image = "cat-2.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 2;
+        $data->image = "cat-3.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 2;
+        $data->image = "cat-4.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 3;
+        $data->image = "small-animal-1.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 3;
+        $data->image = "small-animal-2.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 3;
+        $data->image = "small-animal-3.png";
+        $data->save();
+
+        $data = new Banner();
+        $data->pet_id = 3;
+        $data->image = "small-animal-4.png";
+        $data->save();
+    }
+}
