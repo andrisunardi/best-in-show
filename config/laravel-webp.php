aa16292b386dee2a6a654b395d562d7dac991a59
Andri Sunardi <info@andrisunardi.com>
2024-02-10 02:25:22 +0700

    Update Auth Service Provider - Add View Log Viewer

---
+++ config/laravel-webp.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'default_quality' => 70,
+
+    'default_driver' => 'php-gd',
+
+    'drivers' => [
+
+        'cwebp' => [
+            'path' => '/usr/local/bin/cwebp',
+        ],
+
+        'php-gd' => [
+            //
+        ],
+    ],
+];
