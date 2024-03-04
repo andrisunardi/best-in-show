d1b58a2a433e45ef1af793ec2d8521a31a6e7a13
Andri Sunardi <info@andrisunardi.com>
2024-03-04 07:55:14 +0700

    Product and pet

--- resources/views/livewire/event/index.blade.php
+++ resources/views/livewire/event/index.blade.php
@@ -2,4 +2,115 @@
 @section('icon', 'fas fa-building')
 
 <main>
+    <section id="activitiesBreadcrumb">
+        <div class="container">
+            <div class="breadcrumb">
+                <ol>
+                    <li>
+                        <a href="#">Beranda</a>
+                        <span class="mx-2 font-poppins-m">&gt;</span>
+                    </li>
+                    <li>
+                        <span class="current">Kegiatan Kami</span>
+                    </li>
+                </ol>
+            </div>
+            <div class="my-6">
+                <hr class="border-t-2 border-primaryGray" />
+            </div>
+        </div>
+    </section>
+
+    <section id="activitiesHeader">
+        <div class="global-banner"
+            style="
+          background: linear-gradient(
+            0deg,
+            rgba(0, 0, 0, 0.60) 0%,
+            rgba(0, 0, 0, 0.60) 100%
+          ),
+          url(images/banner/activities-banner.webp)
+          center / cover no-repeat fixed,
+          #D9D9D9;
+        ">
+            <div class="placement">
+                <h1>Kegiatan Kami</h1>
+            </div>
+        </div>
+    </section>
+
+    <section id="activitiesList">
+        <div class="container mt-10">
+            <div class="gridview-activities">
+                <div class="activities-item">
+                    <div class="cover-image"
+                        style="background-image: url(images/thumbnail/thumbnail-category-cat.webp);"></div>
+
+                    <a href="#" class="activity-title">
+                        Best In Show Pameran PERDANA IGSC (INDONESIA GERMAN SHEPHERD COMMUNITY) WILL BANTEN
+                    </a>
+
+                    <div class="my-2">
+                        <hr class="border-t border-[#E0E0E0]" />
+                    </div>
+
+                    <div class="activity-detail">
+                        <i>date_range</i>
+                        <p>28 Oktober 2023</p>
+                    </div>
+
+                    <div class="mt-3 activity-detail">
+                        <i>location_on</i>
+                        <p>Lapangan Von Haus Roman Puncak, Cisarua Bogor</p>
+                    </div>
+                </div>
+
+                <div class="activities-item">
+                    <div class="cover-image"
+                        style="background-image: url(images/thumbnail/thumbnail-category-cat.webp);"></div>
+
+                    <a href="#" class="activity-title">
+                        Best In Show Pameran PERDANA IGSC (INDONESIA GERMAN SHEPHERD COMMUNITY) WILL BANTEN
+                    </a>
+
+                    <div class="my-2">
+                        <hr class="border-t border-[#E0E0E0]" />
+                    </div>
+
+                    <div class="activity-detail">
+                        <i>date_range</i>
+                        <p>28 Oktober 2023</p>
+                    </div>
+
+                    <div class="mt-3 activity-detail">
+                        <i>location_on</i>
+                        <p>Lapangan Von Haus Roman Puncak, Cisarua Bogor</p>
+                    </div>
+                </div>
+
+                <div class="activities-item">
+                    <div class="cover-image"
+                        style="background-image: url(images/thumbnail/thumbnail-category-cat.webp);"></div>
+
+                    <a href="#" class="activity-title">
+                        Best In Show Pameran PERDANA IGSC (INDONESIA GERMAN SHEPHERD COMMUNITY) WILL BANTEN
+                    </a>
+
+                    <div class="my-2">
+                        <hr class="border-t border-[#E0E0E0]" />
+                    </div>
+
+                    <div class="activity-detail">
+                        <i>date_range</i>
+                        <p>28 Oktober 2023</p>
+                    </div>
+
+                    <div class="mt-3 activity-detail">
+                        <i>location_on</i>
+                        <p>Lapangan Von Haus Roman Puncak, Cisarua Bogor</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
--- resources/views/livewire/product/index.blade.php
+++ resources/views/livewire/product/index.blade.php
@@ -2,91 +2,180 @@
 @section('icon', 'fas fa-building')
 
 <main>
-    <section id="aboutHeader">
-        <div class="global-banner"
-            style="
-          background: linear-gradient(
-            0deg,
-            rgba(0, 0, 0, 0.60) 0%,
-            rgba(0, 0, 0, 0.60) 100%
-          ),
-          url({{ asset('assets/images/banner/about-banner.webp') }})
-          center / cover no-repeat fixed,
-          #D9D9D9;
-        ">
-            <div class="placement">
-                <h1>@yield('title')</h1>
-            </div>
-        </div>
-    </section>
-
-    <section id="aboutTabButton">
-        <div class="container mt-12">
-            <div class="grid grid-cols-2 lg:grid-cols-4 gap-6">
-                <button id="btn-aboutVisionMission" type="button" class="btn-about-tab active"
-                    onclick="switchSection('aboutVisionMission')">
-                    {{ trans('index.about_us') }}
-                </button>
-                <button id="btn-aboutPinter" type="button" class="btn-about-tab"
-                    onclick="switchSection('aboutPinter')">
-                    Pinter
-                </button>
-                <button id="btn-aboutDistribution" type="button" class="btn-about-tab"
-                    onclick="switchSection('aboutDistribution')">
-                    Area Distribusi
-                </button>
-                <button id="btn-aboutWarehouse" type="button" class="btn-about-tab"
-                    onclick="switchSection('aboutWarehouse')">
-                    Gudang Kami
-                </button>
-            </div>
-        </div>
-    </section>
-
-    <section id="aboutVisionMission" class="about-section">
+    <section id="productsBreadcrumb">
         <div class="container">
-            <!-- <div class="text-left">
-          <h2 class="page-heading-2 text-primaryBlack">Visi &amp; Misi</h2>
-        </div> -->
-            <div class="mt-6">
-                <img draggable="false" src="{{ asset('assets/images/thumbnail/bisfood-visi-misi.webp') }}"
-                    class="about-items" alt="Visi & Misi Best In Show">
+            <div class="breadcrumb">
+                <ol>
+                    <li>
+                        <a href="#">Home</a>
+                        <span class="mx-2 font-poppins-m">&gt;</span>
+                    </li>
+                    <li>
+                        <a href="#">Anjing</a>
+                        <span class="mx-2 font-poppins-m">&gt;</span>
+                    </li>
+                    <li>
+                        <span class="current">Produk</span>
+                    </li>
+                </ol>
+            </div>
+            <div class="my-6">
+                <hr class="border-t-2 border-primaryGray" />
             </div>
         </div>
     </section>
 
-    <section id="aboutPinter" class="about-section" style="display: none;">
-        <div class="container">
-            <!-- <div class="text-left">
-          <h2 class="page-heading-2 text-primaryBlack">Lorem Ipsum</h2>
-        </div> -->
-            <div class="mt-6">
-                <img src="{{ asset('assets/images/thumbnail/bisfood-pinter.webp') }}" class="about-items"
-                    alt="PINTER">
-            </div>
-        </div>
-    </section>
+    <section id="productsList">
+        <div class="container mt-10">
+            <div class="flex justify-between items-center">
+                <div class="flex items-center gap-2">
+                    <button type="button" class="btn-filter-product" onclick="toggleModal()">
+                        <div class="flex items-center gap-3">
+                            <i class="material-icons rounded-icon text-2xl">tune</i>
+                            <p>Filter</p>
+                        </div>
+                    </button>
 
-    <section id="aboutDistribution" class="about-section" style="display: none;">
-        <div class="container">
-            <!-- <div class="text-left">
-          <h2 class="page-heading-2 text-primaryBlack">Area Distribusi</h2>
-        </div> -->
-            <div class="mt-6">
-                <img src="{{ asset('assets/images/thumbnail/bisfood-distribusi.webp') }}" class="about-items"
-                    alt="Area Distribusi">
-            </div>
-        </div>
-    </section>
+                    <div class="filter-tag">
+                        <div class="flex items-center gap-3">
+                            <p>Super Premium</p>
+                            <button type="button">
+                                <i class="material-icons rounded-icon">close</i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="filter-tag">
+                        <div class="flex items-center gap-3">
+                            <p>Holistic</p>
+                            <button type="button">
+                                <i class="material-icons rounded-icon">close</i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-    <section id="aboutWarehouse" class="about-section" style="display: none;">
-        <div class="container">
-            <!-- <div class="text-left">
-          <h2 class="page-heading-2 text-primaryBlack">Gudang Kami</h2>
-        </div> -->
-            <div class="mt-6">
-                <img src="{{ asset('assets/images/thumbnail/bisfood-warehouse.webp') }}" class="about-items"
-                    alt="Gudang Kami">
+                <div class="hidden lg:block">
+                    <p class="text-primaryGray text-sm font-poppins-r">
+                        Menampilkan 1 &ndash; 16 dari 50 item
+                    </p>
+                </div>
+            </div>
+
+            <div class="mt-10">
+                <div class="relative flex gap-6">
+                    <div class="hidden lg:block w-1/4">
+                        <div class="filter-card">
+                            <form action="" class="product-form">
+                                <h2>Cari Produk</h2>
+
+                                <div class="relative mt-4">
+                                    <div class="absolute top-1/2 left-3 -translate-y-1/2">
+                                        <i class="material-icons rounded-icon">search</i>
+                                    </div>
+                                    <input type="text" placeholder="Cari Produk" />
+                                </div>
+
+                                <div class="relative mt-6">
+                                    <h2>Kategori</h2>
+
+                                    <div class="mt-3">
+                                        <button type="button" class="accordion">
+                                            <div class="flex justify-between items-center">
+                                                <p>Anjing</p>
+                                                <i class="arrow">arrow_drop_down</i>
+                                            </div>
+                                        </button>
+                                        <div class="panel" style="display: none; overflow: hidden;">
+                                            <div class="form-group">
+                                                <label for="">Super Premium</label>
+                                                <input id="" type="checkbox" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Anjing Pameran</label>
+                                                <input id="" type="checkbox" />
+                                            </div>
+                                        </div>
+
+                                        <button type="button" class="accordion">
+                                            <div class="flex justify-between items-center">
+                                                <p>Kucing</p>
+                                                <i class="arrow">arrow_drop_down</i>
+                                            </div>
+                                        </button>
+                                        <div class="panel" style="display: none; overflow: hidden;">
+                                            <div class="form-group">
+                                                <label for="">Super Premium</label>
+                                                <input id="" type="checkbox" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Anjing Pameran</label>
+                                                <input id="" type="checkbox" />
+                                            </div>
+                                        </div>
+
+                                        <button type="button" class="accordion">
+                                            <div class="flex justify-between items-center">
+                                                <p>Hewan Kecil</p>
+                                                <i class="arrow">arrow_drop_down</i>
+                                            </div>
+                                        </button>
+                                        <div class="panel" style="display: none; overflow: hidden;">
+                                            <div class="form-group">
+                                                <label for="">Super Premium</label>
+                                                <input id="" type="checkbox" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Anjing Pameran</label>
+                                                <input id="" type="checkbox" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="w-full lg:w-3/4">
+                        <div class="gridview-product">
+                            <div class="gridview-product-item">
+                                <img src="images/thumbnail/dummy-product.png" alt="Good Dog Puppy Lamb & Rice" />
+                                <div class="text-center">
+                                    <a href="#">Good Dog Puppy Lamb & Rice</a>
+                                    <p>Available 18Kg</p>
+                                </div>
+                            </div>
+                            <div class="gridview-product-item">
+                                <img src="images/thumbnail/dummy-product.png" alt="Good Dog Puppy Lamb & Rice" />
+                                <div class="text-center">
+                                    <a href="#">Good Dog Puppy Lamb & Rice</a>
+                                    <p>Available 18Kg</p>
+                                </div>
+                            </div>
+                            <div class="gridview-product-item">
+                                <img src="images/thumbnail/dummy-product.png" alt="Good Dog Puppy Lamb & Rice" />
+                                <div class="text-center">
+                                    <a href="#">Good Dog Puppy Lamb & Rice</a>
+                                    <p>Available 18Kg</p>
+                                </div>
+                            </div>
+                            <div class="gridview-product-item">
+                                <img src="images/thumbnail/dummy-product.png" alt="Good Dog Puppy Lamb & Rice" />
+                                <div class="text-center">
+                                    <a href="#">Good Dog Puppy Lamb & Rice</a>
+                                    <p>Available 18Kg</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="block lg:hidden mt-10">
+                <div class="text-right">
+                    <p class="text-primaryGray text-sm font-poppins-r">
+                        Menampilkan 1 &ndash; 16 dari 50 item
+                    </p>
+                </div>
             </div>
         </div>
     </section>
