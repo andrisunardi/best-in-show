dcd932266b07a4ec9cfd52015861a9a1db90d70d
Andri Sunardi <info@andrisunardi.com>
2024-03-11 22:08:30 +0700

    Promotion Page

--- app/Livewire/Promotion/PromotionPage.php
+++ app/Livewire/Promotion/PromotionPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire\Promotion;
 
+use App\Models\Promotion;
 use App\Livewire\Component;
 
 class PromotionPage extends Component
 {
+    public function getPromotions()
+    {
+        return Promotion::latest()->active()->get();
+    }
+
     public function render()
     {
-        return view('livewire.promotion.index');
+        return view('livewire.promotion.index', [
+            'promotions' => $this->getPromotions(),
+        ]);
     }
 }
--- resources/views/livewire/promotion/index.blade.php
+++ resources/views/livewire/promotion/index.blade.php
@@ -2,4 +2,59 @@
 @section('icon', 'fas fa-gifts')
 
 <main>
+    <section id="promotionBreadCrumb">
+        <div class="container">
+            <div class="breadcrumb">
+                <ol>
+                    <li>
+                        <a draggable="false" href="{{ route('index') }}">{{ trans('index.home') }}</a>
+                        <span class="mx-2 font-poppins-m">&gt;</span>
+                    </li>
+                    <li>
+                        <span class="current">@yield('title')</span>
+                    </li>
+                </ol>
+            </div>
+            <div class="my-6">
+                <hr class="border-t-2 border-primaryGray" />
+            </div>
+        </div>
+    </section>
+
+    <section id="promotionHeader">
+        <div class="global-banner"
+            style="
+          background: linear-gradient(
+            0deg,
+            rgba(0, 0, 0, 0.60) 0%,
+            rgba(0, 0, 0, 0.60) 100%
+          ),
+          url({{ asset('assets/images/banner/promotion-banner.webp') }})
+          center / cover no-repeat fixed,
+          #D9D9D9;
+        ">
+            <div class="placement">
+                <h1>@yield('title')</h1>
+            </div>
+        </div>
+    </section>
+
+    <section id="promotionList">
+        <div class="container mt-10">
+            <div class="gridview-promotion">
+                @foreach ($promotions as $promotion)
+                    <div class="gridview-promotion-item">
+                        <a draggable="false" href="{{ route('promotion.view', ['slug' => $promotion->slug]) }}">
+                            <img draggable="false" src="{{ $promotion->assetImage() }}"
+                                alt="{{ $promotion->translate_name }}" />
+                            <div class="mt-3">
+                                <p class="promo-date">{{ $promotion->date->isoFormat('LL') }}</p>
+                                <p class="promo-title">{{ $promotion->translate_name }}</p>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 </main>
